6788895376713fbb3ff15c47aad98004ee59eb79
Richard Egeli <richard@Richards-MacBook-Pro.local>
2023-03-07 11:23:37 +0100

    fix: post request also returns download

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 const port = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
-  console.log(req.headers);
+  res.download('./release/app_update.bin');
+});
+
+app.post('/', (req, res) => {
   res.download('./release/app_update.bin');
 });
 
