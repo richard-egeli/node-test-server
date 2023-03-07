f26b174e595f9348efabe722367248d055a75873
Richard Egeli <richard@Richards-MacBook-Pro.local>
2023-03-07 11:37:25 +0100

    feat: logging on the server

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 const port = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
+  console.log('GET request received');
   res.download('./release/app_update.bin');
 });
 
 app.post('/', (req, res) => {
+  console.log('POST request received');
   res.download('./release/app_update.bin');
 });
 
